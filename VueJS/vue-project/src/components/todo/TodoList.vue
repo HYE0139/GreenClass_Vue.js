2c74f0745f08f86892b1388681947dc93d44091a
HYE0139 <h.ye011096@gmail.com>
2022-07-12 20:16:59 +0900

    list 작성 날짜 추가

--- VueJS/vue-project/src/components/todo/TodoList.vue
+++ VueJS/vue-project/src/components/todo/TodoList.vue
@@ -2,10 +2,13 @@
   <section>
     <transition-group name="list" tag="ul">
     <!--반목문 사용을 위해 key값 주기 // v-for == for() -->
-      <li :key="todoItem.key" v-for="todoItem in propsItems" class="shadow">
+      <li :key="todoItem.itodo" v-for="todoItem in propsItems" class="shadow">
         <i class="checkBtn fas fa-check" aria-hidden="true"></i>
-        {{ todoItem.value }}
-        <span class="removeBtn" type="button" @click="removeTodo(todoItem.key)">
+        <div class="grow_1 d-flex flex-col justify_content_evenly">
+          <div class="ctnt">{{ todoItem.todo }}</div>
+          <div class="small_text">{{ todoItem.created_at }}</div>
+        </div>
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem.itodo)">
           <i class="far fa-trash-alt" aria-hidden="true"></i>
         </span>
       </li>
@@ -21,6 +24,7 @@ export default {
     removeTodo(key) {
       this.$emit('childRemoveTodo', key);
     }
+
   },
 
   
@@ -28,13 +32,16 @@ export default {
 </script>
 
 <style scoped>
-  .list-enter-active, .list-leave-active {transition: all 1s;}
-  .list-enter-from, .list-leave-to { opacity: 0; transform: translateY(30px);}
-  ul { list-style: none; padding-left:0; margin-top: 0; text-align: left;}
-  li {
-    display: flex; min-height:50px; height:50px; line-height:50px;
-    margin:0.5rem 0; padding: 0 0.9rem; background-color:white; border-radius:5px;
+  .list-enter-active, .list-leave-active { transition: all 1s; }
+  .list-enter-from, .list-leave-to { opacity: 0; transform: translateY(30px); }
+  ul { list-style-type: none; padding-left: 0; margin-top: 0; text-align: left; }
+  li { 
+    display: flex; margin: 0.5rem 0;
+    padding: 0 0.9rem; background-color: white; border-radius: 5px;
   }
-  .checkBtn { line-height: 50px; color: #62acde; margin-right:10px;}
-  .removeBtn {margin-left:auto; color: #de4343;}
+  .checkBtn { line-height: 50px; color: #62acde; margin-right: 10px; }
+  .removeBtn { line-height: 50px; margin-left: auto; color: #de4343; }
+  .upBtn {line-height: 50px; margin-left: auto; color:#62acde; cursor: pointer;}
+  .small_text { font-size: .8rem; color: #828181; }
+  .upCtnt {border-style:none; border:0; border-bottom: 1px solid #62acde;}
 </style>
