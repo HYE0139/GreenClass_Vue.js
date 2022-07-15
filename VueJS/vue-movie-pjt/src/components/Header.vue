3e11b53b964e6add172bf54987c48b01744bc0cc
HYE0139 <h.ye011096@gmail.com>
2022-07-15 12:13:14 +0900

    router 메뉴

--- VueJS/vue-movie-pjt/src/components/Header.vue
+++ VueJS/vue-movie-pjt/src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <header>
         <nav>
+            <!-- router/index.js 에서 import한 페이지들-->
             <router-link to="/"> Home </router-link> |
             <router-link to="/boxOfficebyDay"> 일별 박스오피스 </router-link> |
             <router-link to="/boxOfficebyWeek"> 주간 박스오피스 </router-link>
