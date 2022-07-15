a0d0c160bb80ae8b2d23775b304e602d6feebe13
HYE0139 <h.ye011096@gmail.com>
2022-07-15 12:08:10 +0900

    Router / WebPage Import

--- VueJS/vue-movie-pjt/src/router/index.js
+++ VueJS/vue-movie-pjt/src/router/index.js
@@ -1,3 +1,9 @@
+// Router
+// 클라이언트의 요청 경로에 따라 해당하는 컴포넌트를 불러와 페이지를 구성하는 곳.
+// URL 변경 시 DOM을 새로 갱신하는 것이 아니라 미리 컴포넌트를 가지고 있다가
+// 변경된 요소영역만 갱신한다.(SPA 언어의 큰 특징)
+
+// view에서 만든 페이지들을 import 
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home';
 import BoxOffice from '../views/BoxOffice';
