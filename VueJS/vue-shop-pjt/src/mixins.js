c294453468ad303c70cffff5d5e540f0cced6157
HYE0139 <h.ye011096@gmail.com>
2022-07-20 18:10:11 +0900

    이미지 파일 삭제

--- VueJS/vue-shop-pjt/src/mixins.js
+++ VueJS/vue-shop-pjt/src/mixins.js
@@ -18,10 +18,17 @@ export default {
                 console.error(e);
             })).data;
         },
-        $base64(file) {
+        async $delete(url, param) {
+            return (await axios.delete(url,{
+                params: param,
+            }).catch(e=> {
+                console.error(e);
+            })).data;
+        },
+        $base64(file) { // 파일을 문자열 형태로 바꾼 다음에 보낸다.
             return new Promise(resolve => {
                 const fr = new FileReader();
-                fr.onload = e => {
+                fr.onload = e => { // onload라는 이벤트를 걸어서 해당 이벤트가 끝나면 함수 내용이 실행됨.
                     resolve(e.target.result);
                 }
                 fr.readAsDataURL(file);
