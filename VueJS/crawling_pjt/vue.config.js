5f79363cb52e46b871799c2a45c4ed92be134022
HYE0139 <h.ye011096@gmail.com>
2022-07-27 21:22:08 +0900

    프록시 설정

---
+++ VueJS/crawling_pjt/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    proxy: { 
+        '/v1': {
+            target: 'https://openapi.naver.com/', 
+            changeOrigin: true 
+        }
+    }
+  }
+})
