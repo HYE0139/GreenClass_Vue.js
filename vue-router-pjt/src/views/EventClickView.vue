bb8d1d5df841543ef8acf095fd4b09b005208d96
HYE0139 <h.ye011096@gmail.com>
2022-07-28 16:53:52 +0900

    클릭 이벤트

---
+++ vue-router-pjt/src/views/EventClickView.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>EventClick</h1>
+        <button type="button" @click="increaseCnt(); printConsole()">Add 1</button>
+        <p>The Counter is : {{ cnt }}</p>
+    </div>
+    <div>
+        {{ selectedValue }}
+        <select v-model="selectedValue" @change="changeSelect">
+            <option>서울</option>
+            <option>부산</option>
+            <option>제주</option>
+        </select>
+    </div>
+    <div>
+        {{ txtVal }}
+        <input type="txtVal" v-model="txtVal" @change="changeTxtVal">
+        <!---->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            cnt:0,
+            selectedValue: '',
+        }
+    },
+    methods: {
+        
+        increaseCnt() {
+            this.cnt++;
+        },
+        printConsole() {
+            console.log(this.cnt);
+        },
+        changeSelect(){
+            alert(this.selectedValue);
+        },
+        changeTxtVal(){
+            console.log(this.txtVal);
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
