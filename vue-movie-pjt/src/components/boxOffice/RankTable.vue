1fadaeb407dddaf41c83ecd25f160b16554f71f5
HYE0139 <h.ye011096@gmail.com>
2022-07-28 12:39:19 +0900

    영화 리스트 thead

---
+++ vue-movie-pjt/src/components/boxOffice/RankTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>순위</th>
+        <th>제목</th>
+        <th>개봉일</th>
+        <th>누적관객수</th>
+        <th>누적매출액</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- 부모 boxOffice Day/week에서 받아온 list 를 for문 돌리고 -->
+      <!-- 자식요소인 RankRow 에 item:Object 을 보내줌-->
+        <RankRow :items="item" v-for="(item) in list" :key="item.movieCd"/>  
+    </tbody>
+  </table>
+</template>
+
+<script>
+import RankRow from './RankRow.vue';
+export default {
+    props: {
+        list: Array
+    },
+    components: {
+        RankRow
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
