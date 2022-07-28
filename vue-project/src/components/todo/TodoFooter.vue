acd011598b8c8c2b21dd08d8462741b9e5825b12
HYE0139 <h.ye011096@gmail.com>
2022-07-28 16:48:15 +0900

    all clear 버튼

---
+++ vue-project/src/components/todo/TodoFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" @click="clearTodo">Clear All</span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clearTodo() {
+      this.$emit('clearTodo');
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .clearAllContainer {
+    width: 8.5rem; height:50px; line-height:50px; background-color: white;
+    border-radius: 5px; margin: 0 auto; cursor: pointer;
+  }
+
+  .clearAllBtn { display: block; border-radius: 5px;}
+  .clearAllBtn:hover { color: white; background: linear-gradient(to right, #6478FB, #8763FB);}
+</style>
