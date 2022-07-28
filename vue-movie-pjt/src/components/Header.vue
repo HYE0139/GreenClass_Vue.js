af6fd5fd7c973c256125507b3fb88095a01a7be2
HYE0139 <h.ye011096@gmail.com>
2022-07-28 12:37:41 +0900

    template

---
+++ vue-movie-pjt/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <nav>
+            <!-- router/index.js 에서 import한 페이지들-->
+            <router-link to="/"> Home </router-link> |
+            <router-link to="/boxOfficebyDay"> 일별 박스오피스 </router-link> |
+            <router-link to="/boxOfficebyWeek"> 주간 박스오피스 </router-link>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
