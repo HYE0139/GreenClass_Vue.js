7815b78854cf3ce4e22109b481b118cd21bbdb1a
HYE0139 <h.ye011096@gmail.com>
2022-07-28 12:40:27 +0900

    Movie-project

---
+++ vue-movie-pjt/src/App.vue
@@ -0,0 +1,52 @@
+<!--
+  view 의 요소들이 합쳐지는 곳
+-->
+<template>
+  <div>
+    <Header /> 
+    <router-view/> 
+    <Footer />
+  </div>
+  
+  
+</template>
+<script>
+  import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
+
+  export default {
+    components: {
+      Header,
+      Footer
+    }
+  }
+
+</script>
+
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+table { border-collapse: collapse; border:1px solid #2c3e50; display: inline-block;}
+th,td { border:1px solid #2c3e50; padding:10px 15px;}
+</style>
